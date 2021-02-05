edf18a52a9ffee7bfd8fb28078753fd32d1864ab
Peter Sturc <sturc.peter@icloud.com>
2021-02-05 15:17:59 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Peter Šturc on 05/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
