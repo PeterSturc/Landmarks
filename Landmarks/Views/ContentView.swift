9f475a206201ece2998a0fae9cd7c765bef3f537
Peter Sturc <sturc.peter@icloud.com>
2021-02-17 21:28:36 +0100

    Swift UI Essentials: Handling User Input

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Peter Šturc on 05/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
